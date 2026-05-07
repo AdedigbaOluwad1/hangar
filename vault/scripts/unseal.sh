efdee4e74b1fc610fee2edb207d37d0787ebe9b1
Oluwadarasimi <boatech4good@gmail.com>
2026-05-08 00:02:20 +0100

    fix: unseal vault on subsequent runs in setup.yml

--- vault/scripts/unseal.sh
+++ vault/scripts/unseal.sh
@@ -4,9 +4,9 @@ export VAULT_SKIP_VERIFY=true
 
 INIT_FILE=/etc/vault.d/keys/init.json
 
-KEY1=$(jq -r '.unseal_keys_b64[0]' $INIT_FILE)
-KEY2=$(jq -r '.unseal_keys_b64[1]' $INIT_FILE)
-KEY3=$(jq -r '.unseal_keys_b64[2]' $INIT_FILE)
+KEY1=$(sudo jq -r '.unseal_keys_b64[0]' $INIT_FILE)
+KEY2=$(sudo jq -r '.unseal_keys_b64[1]' $INIT_FILE)
+KEY3=$(sudo jq -r '.unseal_keys_b64[2]' $INIT_FILE)
 
 vault operator unseal $KEY1
 vault operator unseal $KEY2
